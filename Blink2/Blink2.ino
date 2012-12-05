3c54900e597a2242778bfc31386b683e95f6957e
alastairhm <alastair@montgomery.me.uk>
2012-12-05 16:33:10 +0000

    Blink Two LED binary counter

---
+++ Blink2/Blink2.ino
@@ -0,0 +1,31 @@
+
+int led1 = 8;
+int led2 = 9;
+int counter = 0;
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);  
+}
+
+void loop() {
+
+  if (counter & 1) {
+    digitalWrite(led1, HIGH);
+  }
+  else {
+    digitalWrite(led1, LOW);
+  }
+
+  if (counter & 2) {
+    digitalWrite(led2, HIGH);
+  }
+  else {
+    digitalWrite(led2, LOW);
+  }
+  delay(1000);
+  counter++;
+
+}
+
