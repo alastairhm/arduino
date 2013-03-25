1465610e9cda976ec2ebd9b0223e8aae72530aa0
alastairhm <alastair@montgomery.me.uk>
2013-03-25 16:52:59 +0000

    Passing a parameter containing the message to the test script.

--- SerialMessage/test.rb
+++ SerialMessage/test.rb
@@ -1,6 +1,19 @@
+#!/usr/bin/env ruby
 require "serialport"
+require "optparse"
 
+opts = OptionParser.new
+opts.on("-m Message","--message Message", String, "Message to Send") {|v| @message = v}
+begin
+	opts.parse!(ARGV)
+rescue OptionParser::ParseError => e
+	puts e
+end
+raise OptionParser::MissingArgument, "Message [-m]" if @message.nil?
+
+puts "Initializing Serial Port"
 myport = SerialPort.new("/dev/ttyACM0","9600".to_i)
-sleep(10)
-myport.write(">Hello From Ruby Script<")
+sleep(2)
+puts "Sending message = #{@message}"
+myport.write(@message)
 myport.close
