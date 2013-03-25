a880858edfe674f14d46b2f73df28f8f5142bf0d
alastairhm <alastair@montgomery.me.uk>
2013-03-25 16:35:02 +0000

    Updates to be able to scroll two lines

--- SerialMessage/SerialMessage.ino
+++ SerialMessage/SerialMessage.ino
@@ -1,7 +1,7 @@
 // Auto scroll a message on the LCD and receive new messages via Serial port.
 // Alastair Montgomery (c) 2013
-// Version 1.02
- 
+// Version 1.03
+
 // include the library code:
 #include <LiquidCrystal.h>
 #include <string.h>
@@ -14,11 +14,14 @@ const int ledWidth = 16;
 const int ledDepth = 2;
 
 //Default Message
-char msg[256] = "Serial Message Version 1.02";
+char msg[256] = "Serial Message Version 1.03";
+char info[256] = "New Message";
 char buffer[256];
 int newMsg = true;
-int previous = 0;
-int pos = 0;
+int previous[ledDepth] = {
+  0};
+int pos[ledDepth] = {
+  0};
 
 void setup() {
   // set up the LCD's number of columns and rows: 
@@ -26,42 +29,43 @@ void setup() {
   Serial.begin(9600);
 }
 
-void printLine(int refreshSeconds, int lineNum){
+void printLine(int refreshSeconds, int lineNum, char message[]){
   //Check if the current second since restart is a mod of refresh seconds , 
   //if it is then update the display , it must also not equal the previously 
   //stored value to prevent duplicate refreshes
-  if((millis()/1000) % refreshSeconds == 0 && previous != (millis()/1000)){
-    previous =  (millis()/1000);  //Store the current time we entered for comparison on the next cycle
+  if((millis()/1000) % refreshSeconds == 0 && previous[lineNum] != (millis()/1000)){
+    previous[lineNum] =  (millis()/1000);  //Store the current time we entered for comparison on the next cycle
     lcd.setCursor(0, lineNum);    //Set our draw position , set second param to 0 to use the top line
     char lcdTop[ledWidth];              //Create a char array to store the text for the line
     int copySize = ledWidth;            // What is the size of our screen , this could probably be moved outside the loop but its more dynamic like this
-    if(strlen(msg) < copySize)
+    if(strlen(message) < copySize)
     {
-        //if the msg is smaller than the current buffer use its length instead;
-        copySize = strlen(msg);
+      //if the msg is smaller than the current buffer use its length instead;
+      copySize = strlen(message)+1;
     }
     //Store the current position temporarily and invert its sign if its negative since we are going in reverse
-    int tempPos = pos;
+    int tempPos = pos[lineNum];
     if(tempPos < 0)
     {
-        tempPos = -(tempPos);
+      tempPos = -(tempPos);
     }
     //Build the lcd text by copying the required text out of our template msg variable 
-    memcpy(&lcdTop[0],&msg[tempPos],copySize);
+    memcpy(&lcdTop[0],&message[tempPos],copySize);
     lcd.print(lcdTop);            //Print it from position 0
     //Increase the current position and check if the position + 16 (screen size) would be larger than the msg length , if it is go in reverse by inverting the sign.
-    pos += 1;
-    if(pos +copySize >= strlen(msg))
-    {
-      pos = -(pos);
-      lcd.setCursor(0, 0);  
-      lcd.print("           ");
+    if (copySize == ledWidth){
+      pos[lineNum] += 1;
+      if(pos[lineNum] +copySize >= strlen(msg))
+      {
+        pos[lineNum] = -(pos[lineNum]);
+      }
     }
   }
 }
 
-void loop() {
-  
+int serialRead(char message[]) {
+  int flag = false;
+
   //Read serial
   if (Serial.available()){
     delay(100);
@@ -70,19 +74,26 @@ void loop() {
       buffer[i++] = Serial.read();
     }
     buffer[i] = '\0';
-    strcpy(msg,buffer);
-    newMsg = true;
-    pos = 0;
+    strcpy(message,buffer);
+    flag = true;
   }
- 
+  return flag;  
+}
+
+void loop() {
+
+  newMsg = serialRead(msg);
+
   if (newMsg){
     lcd.noAutoscroll();
     lcd.clear();
     newMsg = false;
-    lcd.setCursor(0, 0);  
-    lcd.print("New Message");    
+    strcpy(info,"New Message"); 
+    pos = {0,0};     
   }
- 
-  printLine(1,1);
+
+  printLine(1,0,info);
+  printLine(1,1,msg);
 }
+
 
